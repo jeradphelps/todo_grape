8709fead869a1ed48772d2e1bb11cbf4fa5c31a0
Jerad Phelps <jerad.phelps@gmail.com>
2014-04-02 15:26:29 -0500

    checkpoint on the client side auth.  working, needs tests.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,115 @@
 var app = angular.module("Todo", ["ngResource"]);
 
+
+
+// blah refactor for a parent scope
+// in there add a flash message set method
+
+// Do we really need a service here?
+// This is a service, so it is a singleton....
+app.service("AccessToken", function() {
+  // return {
+  //   getToken: function() {
+  //     return sessionStorage.getItem("token");
+  //   },
+  //   setToken: function(token) {
+  //     sessionStorage.setItem("token", token);
+  //   },
+  //   haveToken: function() {
+  //     if(this.getToken() != null ) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  // }
+
+  this.token = "";
+  this.getToken = function() {
+    console.log("getting token..."+this.token);
+    return this.token;
+  }
+  this.setToken = function(token) {
+    this.token = token;
+  }
+  this.haveToken = function() {
+    if(this.token != "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  this.destroyToken = function() {
+    this.token = "";
+  }
+});
+
 // Create a Todo resource to match our Todo api endpoints
-app.factory("Todo", function($resource) {
+app.factory("Todo", function($resource, AccessToken) {
+  console.log("aaaaa"+AccessToken.getToken())
   return $resource("/api/todos/:id", 
-    { id: "@id" },
+    { id: "@id",
+      access_token: function() {
+        return AccessToken.getToken();
+      }
+    },
     { update: { method: "PUT" } }
   );
 });
 
+this.AccessTokenController = function($scope, $http, AccessToken) {
+
+  // Let's prefill the form for ease of use in this case.
+  $scope.newAccessToken = {
+    username: "username",
+    password: "password"
+  }
+
+  $scope.add = function() {
+    if($scope.newAccessToken == undefined 
+      || $scope.newAccessToken.username == "" 
+      || $scope.newAccessToken.password == "") {
+      $scope.$parent.message = "Please Enter Your Credentials.";
+      $scope.$parent.messageClass = $scope.$parent.ERROR_MESSAGE_CLASS;
+      return;
+    }
+
+    $http.post('/api/access_tokens', { username: $scope.newAccessToken.username, password: $scope.newAccessToken.password })
+      .success(function(response) {
+        AccessToken.setToken(response.token);
+        $scope.$parent.message = "Success!  You Are Logged In!";
+        $scope.$parent.messageClass = $scope.$parent.SUCCESS_MESSAGE_CLASS;
+      })
+      .error(function() {
+        $scope.$parent.message = "Invalid Username Or Password.";
+        $scope.$parent.messageClass = $scope.$parent.ERROR_MESSAGE_CLASS;
+      })
+  }
+
+  $scope.haveToken = function() {
+    return AccessToken.haveToken();
+  }
+
+  $scope.destroyToken = function() {
+    return AccessToken.destroyToken();
+  }
+
+  $scope.getToken = function() {
+    return AccessToken.getToken();
+  }
+
+}
+
 this.TodoController = function($scope, Todo) {
-  var ERROR_MESSAGE_CLASS = "error-message";
-  var EMPTY_MESSAGE_CLASS = "empty-message";
-  var SUCCESS_MESSAGE_CLASS = "success-message";
+  $scope.ERROR_MESSAGE_CLASS = "error-message";
+  $scope.EMPTY_MESSAGE_CLASS = "empty-message";
+  $scope.SUCCESS_MESSAGE_CLASS = "success-message";
 
   $scope.todos = Todo.query(
     function() {  },
     function() { 
       $scope.message = "Something went wrong, we could not fetch your todos."; 
-      $scope.messageClass = ERROR_MESSAGE_CLASS;
+      $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
     }
   );
 
@@ -29,7 +121,7 @@ this.TodoController = function($scope, Todo) {
     var todo = Todo.save($scope.newTodo,
       function() { 
         $scope.message = todo.label + " Successfully Created!";
-        $scope.messageClass = SUCCESS_MESSAGE_CLASS;
+        $scope.messageClass = $scope.SUCCESS_MESSAGE_CLASS;
       },
       function() {
         // our save failed
@@ -37,7 +129,7 @@ this.TodoController = function($scope, Todo) {
         $scope.todos.pop(todo);
         $scope.newTodo = { label: todo.label }
         $scope.message = "Something went wrong, we could not create your todo.";
-        $scope.messageClass = ERROR_MESSAGE_CLASS;
+        $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
       }
     );
 
@@ -45,7 +137,7 @@ this.TodoController = function($scope, Todo) {
     $scope.todos.push(todo);
     $scope.newTodo = {};
     $scope.message = "";
-    $scope.messageClass = EMPTY_MESSAGE_CLASS;
+    $scope.messageClass = $scope.EMPTY_MESSAGE_CLASS;
   }
 
   $scope.toggleDone = function(todo) {
@@ -55,11 +147,11 @@ this.TodoController = function($scope, Todo) {
       function() { 
         var doneText = todo.done ? 'Complete' : 'Incomplete';
         $scope.message = todo.label + " Successfully Marked As " + doneText + "!";
-        $scope.messageClass = SUCCESS_MESSAGE_CLASS;
+        $scope.messageClass = $scope.SUCCESS_MESSAGE_CLASS;
       },
       function() {
         $scope.message = "Something went wrong, we could not update your todo.";
-        $scope.messageClass = ERROR_MESSAGE_CLASS;
+        $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
         todo.done = !todo.done;
       }
     );
@@ -72,11 +164,11 @@ this.TodoController = function($scope, Todo) {
     todo.$delete(
       function() { 
         $scope.message = todo.label + " Successfully Deleted!";
-        $scope.messageClass = SUCCESS_MESSAGE_CLASS;
+        $scope.messageClass = $scope.SUCCESS_MESSAGE_CLASS;
       },
       function() { 
         $scope.message = "Something went wrong, we could not delete your todo."; 
-        $scope.messageClass = ERROR_MESSAGE_CLASS;
+        $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
         $scope.todos.push(todo);
       }
     );
@@ -86,7 +178,7 @@ this.TodoController = function($scope, Todo) {
 
     if($scope.newTodo == undefined || $scope.newTodo.label == "") {
       $scope.message = "Todo cannot be blank!";
-      $scope.messageClass = ERROR_MESSAGE_CLASS;
+      $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
       return false;
     }
 
@@ -94,7 +186,7 @@ this.TodoController = function($scope, Todo) {
     for(var i = 0; i < $scope.todos.length; i++) {
       if( $scope.todos[i].label.toLowerCase() == $scope.newTodo.label.toLowerCase() ) {
         $scope.message = "You already have to do " + $scope.newTodo.label + "!";
-        $scope.messageClass = ERROR_MESSAGE_CLASS;
+        $scope.messageClass = $scope.ERROR_MESSAGE_CLASS;
         return false;
       }
     }
