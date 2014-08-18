7018d57174dec2b39ee1a825709229ec46a72f01
Jerad Phelps <jeradphelps@Jerads-MacBook-Pro.local>
2014-08-18 16:27:25 -0600

    pointless comment

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,6 +1,8 @@
 module API
   class Root < Grape::API
 
+    # Do I build?
+
     helpers do
       def authenticate! access_token
         error!('401 Unauthorized', 401) unless AccessToken.find_by(:token => access_token)
