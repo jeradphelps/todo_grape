48775c609863528018bb5b4b8de31befd4707e72
Jerad Phelps <jeradphelps@Jerads-MacBook-Pro.local>
2014-08-19 10:40:25 -0600

    a pr

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,7 +1,7 @@
 module API
   class Root < Grape::API
 
-    # a pull request.7
+    # a pull request.8
     helpers do
       def authenticate! access_token
         error!('401 Unauthorized', 401) unless AccessToken.find_by(:token => access_token)
